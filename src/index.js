d1c15103aca751902052818a46ec508623ba9183
Chubareva <33682126+Chubareva@users.noreply.github.com>
2020-10-22 21:53:03 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -8,19 +8,18 @@ module.exports = function check(str, bracketsConfig) {
 
   let res=[];
   for(let i  of str.split('')){
-    
-    
-      if( keys.includes(i)){
-        res.push(i);
-
-    }
-       else if (values.includes(i)){
+    if (values.includes(i)){
       let a = Object.keys(obj).find(key => obj[key] === i);
         if(res.includes(a)){
-      res.pop(a);
-        }else return false;}
-    
+        res.pop(a);
+        }
+    }
+       else if( keys.includes(i)){
+        res.push(i);
+        }else return false;
   }
+    
+  
 
   return res.length === 0;
   
