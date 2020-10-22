8d2d40322a579f8f08860b87ae8bd0589608a6da
Chubareva <33682126+Chubareva@users.noreply.github.com>
2020-10-22 21:27:49 +0300

    refactor: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+
+  let obj = Object.fromEntries(bracketsConfig);
+
+  let keys = Object.keys(obj);
+  let values = Object.values(obj);
+
+  let res=[];
+  for(let i  of str.split('')){
+    
+    
+      if( keys.includes(i)){
+        res.push(i);
+
+    }
+       else if (values.includes(i)){
+      let a = Object.keys(obj).find(key => obj[key] === i);
+        if(res.includes(a)){
+      res.pop(a);
+        }else return false;}
+    
+  }
+
+  return res.length === 0;
+  
 }
